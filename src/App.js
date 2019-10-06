f5227604c2195b5292fcdeef5b4792c27a0ee660
unknown <poslovnogacic@gmail.com>
2019-10-06 03:22:36 +0200

    commit one

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Recipes from "./pages/Recipes";
 import SingleRecipes from "./pages/SingleRecipes";
 import Default from "./pages/Default";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Navbar from "./components/Navbar";
 class App extends Component {
   render() {
   return (
     <Router >
+      <Navbar />
       <main>
         {/* navbar */}
       <Switch> 

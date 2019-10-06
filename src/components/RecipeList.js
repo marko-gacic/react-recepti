fb60820eab05516960319188e3a5c29ed4283c60
unknown <poslovnogacic@gmail.com>
2019-10-06 18:06:56 +0200

    commit

---
+++ src/components/RecipeList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Recipe from "./Recipe"
+export default class RecepiList extends Component {
+ render() {
+  return (
+   <div>
+    Hello from recipe list
+    <Recipe />
+   </div>
+  )
+ }
+}
